d7e4859d0d8d204c9ecb7ca6df66b7245042f494
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-01-27 18:00:19 +0100

    fix the reviewer required changes

--- enumerable.rb
+++ enumerable.rb
@@ -5,22 +5,18 @@ class MyList
   def initialize(*args)
     @list = args
   end
-
-  def each(&block)
-    @list.each(&block)
-  end
 end
 
 # Create our list
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-puts(list.all?) { |e| e < 5 } # true
-puts(list.all?) { |e| e > 5 } # false
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
 
 # Test #any?
-puts(list.any?) { |e| e == 2 } # true
-puts(list.any?) { |e| e == 5 } # false
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
 
 # Test #filter
 p list.filter(&:even?) # [2, 4]
